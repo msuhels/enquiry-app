b87d0b3a75f07ae66d262370a803b165f63d15d7
msuhels <m.suhel11@gmail.com>
2025-10-29 17:11:40 +0530

    Refactor code structure for improved readability and maintainability

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,7 @@
 import { LoginForm } from "@/components/modules/auth-module/forms/login-form";
 import { Metadata } from "next";
+import Image from "next/image";
+import logo from "@/app/Alzato logo.svg"
 
 export const metadata: Metadata = {
   title: "Login",
@@ -9,7 +11,14 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+      <div className="w-full max-w-sm flex flex-col items-center gap-6">
+        <Image
+          src={logo}
+          alt="Alzato"
+          width={200}
+          height={120}
+          className="object-contain"
+        />
         <LoginForm />
       </div>
     </div>
